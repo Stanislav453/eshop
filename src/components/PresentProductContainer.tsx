fcec4369878f0dca216955708e5037eec3132ead
Stanislav Jendrisak <st.jendrisak@gmail.com>
2026-03-22 10:56:01 +0100

    create component

---
+++ src/components/PresentProductContainer.tsx
@@ -0,0 +1,19 @@
+import { DashboardLayout } from "./common/DashboardLayout";
+
+export const PresentProductContainer = () => {
+
+
+  
+
+  return (
+    <DashboardLayout direction="flex-col">
+      <div>
+        <h2>Our products</h2>
+        <ul>
+          <li>Product 1</li>
+        </ul>
+      </div>
+      <ul></ul>
+    </DashboardLayout>
+  );
+};
