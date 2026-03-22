876c07d895aa84e0b8c662b25802050343931fab
Stanislav Jendrisak <st.jendrisak@gmail.com>
2026-03-22 10:54:19 +0100

    make component more customize

--- src/components/common/DashboardLayout.tsx
+++ src/components/common/DashboardLayout.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 type DashboardLayoutProps = {
-  className?: string;
+  sectionClassName?: string;
+  childrenClassName?: string;
   children: React.ReactNode;
   direction: "flex-row" | "flex-col";
 };
 
 export const DashboardLayout = ({
-  className,
+  sectionClassName,
+  childrenClassName,
   children,
   direction,
 }: DashboardLayoutProps) => {
   return (
-    <section className={className}>
+    <section className={sectionClassName}>
       <div className="container mx-auto p-2">
-        <div className={`max-w-xl flex ${direction} justify-between mx-auto`}>
+        <div className={`max-w-xl flex ${direction} ${childrenClassName} mx-auto`}>
           {children}
         </div>
       </div>
