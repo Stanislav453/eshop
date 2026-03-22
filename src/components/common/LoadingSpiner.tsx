0463f745efedcfe0159d9aa2a5aad8a31e4ad59f
Stanislav Jendrisak <st.jendrisak@gmail.com>
2026-03-22 10:54:30 +0100

    create component

---
+++ src/components/common/LoadingSpiner.tsx
@@ -0,0 +1,21 @@
+import { CircularProgress } from "react-loader-spinner";
+import { DashboardLayout } from "./DashboardLayout";
+
+export const LoadingSpiner = () => {
+  return (
+    <DashboardLayout direction="flex-row" childrenClassName="justify-center">
+
+      <CircularProgress
+        height="50"
+        width="50"
+        color="#333"
+        ariaLabel="circular-progress-loading"
+        wrapperStyle={{}}
+        wrapperClass="wrapper-class"
+        visible={true}
+        strokeWidth={2}
+        animationDuration={1}
+      />
+    </DashboardLayout>
+  );
+};
