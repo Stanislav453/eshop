f29a3128e41a91bb1042fb45916e4c78dc5b0965
Stanislav Jendrisak <st.jendrisak@gmail.com>
2026-04-12 11:21:18 +0200

    better type

--- src/hooks/useFilterData.tsx
+++ src/hooks/useFilterData.tsx
@@ -3,14 +3,12 @@ import axios from "axios";
 import { API_URL } from "../api/apiUrl";
 import type { UseFilterDataType } from "../type";
 
-export const useFilterData = ({ filterValue }: UseFilterDataType) => {
-  console.log(filterValue);
-
+export const useFilterData = <T,>({ filterValue }: UseFilterDataType) => {
   return useQuery({
     queryKey: [filterValue],
-    queryFn: async () => {
+    queryFn: async ():Promise<T> => {
       console.log("slug", `${API_URL}/products?slug=${filterValue}`);
-      const { data } = await axios.get(
+      const { data } = await axios.get<T>(
         `${API_URL}/products?slug=${filterValue}`,
       );
       return data;
