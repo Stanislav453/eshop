c5d24b3125d8152ac98f74f5539aebb5c0ef321c
Stanislav Jendrisak <st.jendrisak@gmail.com>
2026-06-28 12:00:07 +0200

    try vercel

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,6 +5,7 @@ import { ShopHeaderContainer } from "./ShopHeaderContainer";
 export const Header = () => {
   return (
     <header>
+      sdVGsUcvuSDcCbSJHcsDhcbcb
       <DashboardLayout>
         <HeaderTopView />
       </DashboardLayout>
