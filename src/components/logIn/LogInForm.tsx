3de864ffe9634110a6717dd7529620991368255f
Stanislav Jendrisak <st.jendrisak@gmail.com>
2026-06-28 11:47:13 +0200

    refactoring

---
+++ src/components/logIn/LogInForm.tsx
@@ -0,0 +1,37 @@
+    
+const LoginForm = () => {
+  return (
+    <form className="flex flex-col gap-8 mt-8" action="post">
+      <ul className="flex flex-col gap-8 mt-8">
+        <li className="flex flex-col gap-2">
+          <label htmlFor="email">Email</label>
+          <input
+            className="p-4"
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            required
+          />
+        </li>
+        <li className="flex flex-col gap-2">
+          <label htmlFor="password">Password</label>
+          <input
+            className="p-4"
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            required
+          />
+        </li>
+      </ul>
+      <button
+        className="self-center mt-8 px-8 py-4 bg-brand-color text-white font-bold bg-neutral-primary hover:bg-primary-hover cursor-pointer transition-colors duration-300"
+        type="submit"
+      >
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/logIn/SignUpPrompt.tsx
@@ -0,0 +1,17 @@
+import { Link } from "@tanstack/react-router";
+
+export const SignUpPrompt = () => {
+  return (
+    <div className="flex justify-center mt-8">
+      <p className="text-neutral-primary">
+        Don't have an account?{" "}
+        <Link
+          className="text-brand-color font-bold hover:text-primary-hover cursor-pointer transition-colors duration-300"
+          to="/sign-up"
+        >
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  );
+};
